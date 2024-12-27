05374fcda44de6641d468dd7037653eea919566e
Roverz123 <roverz123official@gmail.com>
2024-12-26 18:30:27 -0800

    I can't add music rn(skill issue(give me a break I'm just learning javascript)) so I added the other pages without their conteent

--- public/scripts/burger-menu.js
+++ public/scripts/burger-menu.js
@@ -1,3 +1,4 @@
+//Thank you Treehouse https://youtube.com/watch?v=aNDqzlAKmZc
 const burgerButton = document.querySelector('.burger-button');
 burgerButton.addEventListener('click', () => {
     burgerButton.classList.toggle('open');
